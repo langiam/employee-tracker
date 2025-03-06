881dcb0b962e7997977b244002bb599ee9a372d8
langiam <ryan.matthew.lang@gmail.com>
2025-03-06 09:40:05 -0500

    properly seeded data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,43 +1,47 @@
+-- Active: 1741267800011@@127.0.0.1@5432@employee_tracker
 INSERT INTO department (name) VALUES
     ('Legal'),
     ('Marketing'),
     ('Sales'),
-    ('IT');
-    ('HR');
+    ('IT'),
+    ('HR'),
     ('Finance');
 
 INSERT INTO role (title, salary, department_id) VALUES
-    ('Legal', 250000, 1),
-    ('Marketing', 145000, 2),
-    ('Sales', 90000, 3),
-    ('IT', 75000, 4),
-    ('HR', 145000, 5),
-    ('Finance', 200000, 6);
+    ('Corporate Lawyer', 250000, 1),  -- Legal
+    ('Brand Strategy', 145000, 2),    -- Marketing
+    ('Account Executive', 90000, 3),  -- Sales
+    ('IT Specialist', 75000, 4),      -- IT
+    ('HR Officer', 145000, 5),        -- HR
+    ('Financial Analyst', 200000, 6); -- Finance
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
     ('Ted', 'Roberts', 1, NULL),
     ('Elise', 'Himmel', 1, 1),
     ('Sally', 'Hill', 1, NULL),
     ('John', 'Doe', 1, NULL),
+
     ('Zara', 'Brown', 2, NULL),
     ('Cecilia', 'Ruiz', 2, 2),
     ('Eva', 'Davis', 2, NULL),
     ('Adam', 'Smith', 2, NULL),
+
     ('Sofia', 'Garcia', 3, NULL),
     ('Luis', 'Sanchez', 3, 3),
     ('Fernando', 'Jones', 3, NULL),
     ('Sarah', 'Taylor', 3, NULL),
+
     ('Julian', 'Martinez', 4, NULL),
     ('Mateo', 'Gonzalez', 4, 4),
     ('Olivia', 'Brown', 4, NULL),
     ('Ethan', 'Miller', 4, NULL),
+
     ('Savannah', 'Rodriguez', 5, NULL),
     ('Ava', 'Hernandez', 5, 5),
     ('Gabriel', 'Williams', 5, NULL),
     ('Lily', 'Martin', 5, NULL),
+
     ('Alex', 'Lopez', 6, NULL),
     ('Isabella', 'Gomez', 6, 6),
     ('Elijah', 'Hall', 6, NULL),
-    ('Charlotte', 'Thomas', 6, NULL),
-
-
+    ('Charlotte', 'Thomas', 6, NULL);
