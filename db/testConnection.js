cba8702e439cf658221fa2449b5a3d22b8bbdb0b
langiam <ryan.matthew.lang@gmail.com>
2025-03-06 10:06:41 -0500

    fixed node not working with .env

--- db/testConnection.js
+++ db/testConnection.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const pool = require('./connection');
 
 async function testDB() {
-    try {
-        const res = await pool.query('SELECT * FROM department;');
-        console.log('Database successfuly connected! Departments:', res.rows);
-    } catch (error) {
-        console.error('Database connection error detected:', error);
-    } finally {
-        pool.end();
-    }
+  try {
+    const res = await pool.query('SELECT * FROM department;');
+    console.log('Database connected! Departments:', res.rows);
+  } catch (error) {
+    console.error('Database connection error:', error);
+  } finally {
+    pool.end();
+  }
 }
 
 testDB();
