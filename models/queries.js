34beb7526201916418b1dc0548bece4ba9a332df
langiam <ryan.matthew.lang@gmail.com>
2025-03-06 13:02:12 -0500

    updating queries language

--- models/queries.js
+++ models/queries.js
@@ -13,15 +13,44 @@ class Database {
     }
 
 // View All Depts.
-
-
+static async viewRoles()
+    const result = await pool.query(`
+        SELECT role.id, role.title, role.salary, department.name AS department
+        FROM role
+        JOIN department ON role.department_id = department.id;
+        `);
+    return result.rows;
+}
 
 // View all roles
 
+static async viewRoles() {
+    const result = await pool.query(`
+        SELECT role.id, role.title, role.salary, department.name AS department
+        FROM role
+        JOIN department ON role.department_id = department.id;
+        `);
+    return result.rows;
+}
 // View all employees
 
+static async viewEmployees() {
+    const result = await pool.query(`
+        SELECT e.id, e.fist_name, role.title AS job_title, department.name AS department, 
+            role.salary, COALESCE(m.first_name || ' ' || m.last_name, 'None') AS manager
+        FROM employee e
+        JOIN role ON e.role_id = role.id
+        JOIN department ON role.department_id = department.id
+        LEFT JOIN employee m ON e.manager_id = m.id;
+        `);
+    return result.rows;
+}
+    
 // Add a department
 
+static async addDepartment(name) {
+    await pool.query(`INSERT INTO department (name) VALUES ($1);`, [name]);
+}
 // Add role
 
 // Add employee
